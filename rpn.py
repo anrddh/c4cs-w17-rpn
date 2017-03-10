73f3b179287256d021e3eae84f4f387899b6a14a
Aniruddh Agarwal <aaniruddh99@gmail.com>
2017-03-10 14:46:29 -0500

    Make the subtact test pass

--- rpn.py
+++ rpn.py
@@ -1,12 +1,25 @@
 #!/usr/bin/env python3
 
+import operator
+
+OPERATORS = {
+    '+': operator.add,
+    '-': operator.sub,
+}
+
 def calculate(arg):
     stack = []
     for operand in arg.split():
-        if operand == '+':
-            stack.append(stack.pop() + stack.pop())
-        else:
-            stack.append(float(operand))
+        try:
+            operand = float(operand)
+            stack.append(operand)
+        except:
+            arg2 = stack.pop()
+            arg1 = stack.pop()
+            operator_fn = OPERATORS[operand]
+            result = operator_fn(arg1, arg2)
+
+            stack.append(result)
     return stack.pop()
 
 def main():
